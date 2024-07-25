c0594cd801b574ad68e45be0982107153500cbdb
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2024-07-25 13:57:30 +0300

    Added dashboard  route and page.tsx with basic stracture

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,9 @@ import Logo from "../public/ronny-logo.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-5">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-5 px-5 py-10">
       <header id="header" className="flex flex-row gap-5 justify-evenly w-full">
         <button>LOGO</button>
-        <div className="flex flex-row gap-5">
-          <button>Overview</button>
-          <button>Requests</button>
-          <button>Visuals</button>
-        </div>
       </header>
 
       <section id="input-section" className="mt-16 flex-1">
