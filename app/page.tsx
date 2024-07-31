41f15d37991ca19ce7b8819800aa847baf091195
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2024-07-31 20:27:10 +0300

    logManipulation logic for first store the data in Convex db

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,27 @@
-"use client";
-import { useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
+// "use client";
+// import { useQuery } from "convex/react";
+// import { api } from "../convex/_generated/api";
+// import { useEffect, useState } from "react";
+// import { GET } from "./api/route";
+// import Logo from "../public/ronny-logo.svg";
+// import { logManipultaion } from "@/lib/logManipulation";
 
-import Logo from "../public/ronny-logo.svg";
+import { logManipulation } from "@/lib/logManipulation";
 
 export default function Home() {
-  const plantarrayLog = useQuery(api.utils.plantarrayLog.get);
-  console.log(plantarrayLog);
-  
+  // const plantarrayLog = useQuery(api.utils.plantarrayLog.get);
+  const data = logManipulation();
+  console.log(data[1000]);
+
+  // const { id, time } = data[100];
+  // const { id, time } = second;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-5 px-5 py-10">
       <header id="header" className="flex flex-row gap-5 justify-evenly w-full">
         <button>LOGO</button>
       </header>
-
-      <section id="input-section" className="mt-16 flex-1">
-        <form className="border-2 flex flex-col gap-5 items-center w-96 ">
-          <label htmlFor="autocomplete">Search Query</label>
-          <input id="autocomplete" type="text"></input>
-          <div id="chips">
-            <div className="flex flex-row gap-4 ">
-              <p>example</p>
-              <p>example</p>
-              <p>example</p>
-            </div>
-            <div className="flex flex-row gap-4 justify-center">
-              <p>example</p>
-              <p>example</p>
-            </div>
-          </div>
-          <button type="submit">icon - FILTER QUERIES</button>
-        </form>
-      </section>
+      {/* 0 */}
 
       <footer className="">
         <p>Designed and Built by</p>
---
+++ lib/logManipulation.ts
@@ -0,0 +1,150 @@
+import * as fs from "fs";
+import path from "path";
+
+interface ProcessedRequest {
+  id: number;
+  time: string;
+  request: ProcessedLineTitle;
+  details: ProcessedLineTitle[];
+  error?: unknown;
+}
+
+interface ProcessedLineTitle {
+  title: string;
+  content: string;
+  kind: string;
+}
+
+// Function to split log file based on custom delimiter condition
+function splitLogByDelimiter(logData: string): string[] {
+  const lines = logData.split("\n");
+  const delimiterIndices: number[] = [];
+
+  const delimiter =
+    "------------------------------------------------------------------------------------------------------------";
+
+  // Identify rows that should be used as delimiters
+  lines.forEach((line, index) => {
+    const parts = line.split(" - ");
+    if (parts[2] && parts[2].trim() === delimiter) {
+      delimiterIndices.push(index);
+    }
+  });
+
+  // Split the log content based on identified delimiter rows
+  const logParts: string[] = [];
+  let start = 0;
+  delimiterIndices.forEach((delimiterIndex) => {
+    logParts.push(lines.slice(start, delimiterIndex).join("\n"));
+    start = delimiterIndex + 1;
+  });
+
+  // Include the last part if there is any remaining content
+  if (start < lines.length) {
+    logParts.push(lines.slice(start).join("\n"));
+  }
+
+  return logParts;
+}
+
+export function logManipulation(): ProcessedRequest[] {
+  const allProcessedRequests: ProcessedRequest[] = [];
+  let requestId = 1;
+
+  try {
+    const filePath = path.join(
+      process.cwd(),
+      "public",
+      "assets",
+      "files",
+      "plantarray.log"
+    );
+    const logData = fs.readFileSync(filePath, "utf8");
+    const requests = splitLogByDelimiter(logData);
+
+    for (const request of requests) {
+      const requestObject = requestManipulation(request, requestId);
+      allProcessedRequests.push(requestObject);
+      requestId++;
+    }
+  } catch (error) {
+    console.error("There has been an error: " + error);
+  }
+
+  return allProcessedRequests;
+}
+
+function requestManipulation(request: string, id: number): ProcessedRequest {
+  const requestLines = request.split("\n");
+
+  if (requestLines.length < 2) {
+    return {
+      id,
+      time: "Invalid Time",
+      request: {
+        title: "Invalid Title or Request Format",
+        content: "Invalid Content or Request Format",
+        kind: "Invalid Kind or Request Format",
+      },
+      details: [
+        {
+          title: "Invalid Title or Request Format",
+          content: "Invalid Content or Request Format",
+          kind: "Invalid Kind or Request Format",
+        },
+      ],
+    };
+  }
+
+  try {
+    const startLine = requestLines[0];
+    const [time, kind, lineTitle] = startLine.split(" - ");
+
+    const hasStartRequest = lineTitle.includes("START REQUEST: ");
+    const splitSign = hasStartRequest ? ": " : "";
+    const [title, content] = lineTitle.split(splitSign);
+
+    const details: ProcessedLineTitle[] = [];
+
+    for (let i = 1; i < requestLines.length; i++) {
+      const detailsLineWithFullDetails = requestLines[i];
+      const [detailsTime, detailsKind, detailsLineTitle] =
+        detailsLineWithFullDetails.split(" - ");
+      const detailsSplitSign = hasStartRequest ? ": " : " --> ";
+
+      const [detailsTitle, detailsContent] =
+        detailsLineTitle.split(detailsSplitSign);
+
+      details.push({
+        title: detailsTitle || "Invalid Title",
+        content: detailsContent || "Invalid Content",
+        kind: detailsKind || "Invalid Kind",
+      });
+    }
+
+    return {
+      id,
+      time: time || "Invalid Time",
+      request: {
+        title: title || "Invalid Title",
+        content: content || "Invalid Content",
+        kind: kind || "Invalid Kind",
+      },
+      details,
+    };
+  } catch (error) {
+    return {
+      id,
+      time: "unknown",
+      request: { title: "unknown", content: "unknown", kind: "unknown" },
+      details: [
+        {
+          title: "unknown",
+          content: "unknown",
+          kind: "unknown",
+        },
+      ],
+      error: "This is the error the was found: " + error,
+    };
+  }
+}
